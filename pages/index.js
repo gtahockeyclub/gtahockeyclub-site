6f61b12f612866c42f3d7029b4fbcfbaad64899d
gtahockeyclub <gtahockeyclub@gmail.com>
2026-05-05 13:36:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,19 +55,10 @@ export default function Home() {
   }
 
 const loadGames = async () => {
-  const now = new Date()
-
-  const year = now.getFullYear()
-  const month = String(now.getMonth() + 1).padStart(2, '0')
-  const day = String(now.getDate()).padStart(2, '0')
-
-  const todayLocal = `${year}-${month}-${day}`
-
   const { data, error } = await supabase
     .from('games')
     .select('*')
     .eq('is_active', true)
-    .gte('game_date', todayLocal)
     .order('game_date', { ascending: true })
     .order('game_time', { ascending: true })
 
