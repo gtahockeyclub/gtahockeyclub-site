c1390b26003218802cbd5357e20809d964a171b4
gtahockeyclub <gtahockeyclub@gmail.com>
2026-05-10 23:00:54 -0400

    Built initial Organizer Dashboard layout

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,13 @@
 export default function Dashboard() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div style={{ padding: "40px", color: "white" }}>
       <h1>Organizer Dashboard</h1>
+
+      <div style={{ marginTop: "30px" }}>
+        <button>My Games</button>
+        <button style={{ marginLeft: "10px" }}>Post Game</button>
+        <button style={{ marginLeft: "10px" }}>Manage Players</button>
+      </div>
     </div>
   )
 }
