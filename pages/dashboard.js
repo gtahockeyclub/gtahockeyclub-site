6d6b2d9dc8a55af31994c8f14da4eda3a2579dd7
gtahockeyclub <gtahockeyclub@gmail.com>
2026-05-22 01:17:53 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -84,7 +84,7 @@ console.log(user)
       team2_name: team2Name || 'Team 2',
       organizer_name: organizer,
       organizer_email: organizerEmail,
-      organizer_id: organizerEmail,
+      organizer_id: null,
       is_active: true
     },
   ])
