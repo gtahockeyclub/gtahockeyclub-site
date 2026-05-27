5a00ca1236ab1a145419cef1b04f2424077889c2
gtahockeyclub <gtahockeyclub@gmail.com>
2026-05-27 00:19:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,73 @@ export default function Home() {
   <>
     <Navbar />
 
-    <div style={{
-      padding: "40px",
-      fontFamily: "Arial"
-    }}>
-      <h1>GTA Hockey Club</h1>
+    <div
+  style={{
+    minHeight: "700px",
+    backgroundImage: 'url("/hero-hockey-bg.png.png")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    alignItems: "center",
+    padding: "60px",
+    boxSizing: "border-box"
+  }}
+>
 
-      <p>
-        Homepage working.
-      </p>
+  <div
+    style={{
+      maxWidth: "650px"
+    }}
+  >
 
-            <p>
-        Loaded: {loaded ? "YES" : "NO"}
-      </p>
+    <h1
+      style={{
+        fontSize: "96px",
+        lineHeight: "0.9",
+        margin: 0,
+        fontWeight: "900"
+      }}
+    >
+      <span style={{ color: "#D62828" }}>
+        POST.
+      </span>
 
-    </div>
+      <br />
+
+      <span style={{ color: "#0B1F3A" }}>
+        CONNECT.
+      </span>
+
+      <br />
+
+      <span style={{ color: "#F4B400" }}>
+        PLAY.
+      </span>
+    </h1>
+
+    <h2
+      style={{
+        fontSize: "36px",
+        marginTop: "30px",
+        marginBottom: "10px",
+        color: "#07152b"
+      }}
+    >
+      ONE HOCKEY COMMUNITY ACROSS THE GTA
+    </h2>
+
+    <p
+      style={{
+        fontSize: "20px",
+        color: "#374151"
+      }}
+    >
+      Built for players and organizers.
+    </p>
+
+  </div>
+
+</div>
   </>
 )
 }
