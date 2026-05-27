a09e04d8c61b18ae7ddcdf963b7709293b320e83
gtahockeyclub <gtahockeyclub@gmail.com>
2026-05-27 00:53:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,6 @@ const { error } = await supabase
   .from("game_signups")
   .insert([
     {
-   {
   game_id: gameId,
   game_name: `${game.arena} - ${game.game_date} ${game.game_time}`,
   player_name: user.email,
